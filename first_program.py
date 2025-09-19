ba9d9dfda6b66d080c7fb02aa1f45355e6a2186e
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2025-09-19 16:04:52 -0400

    initial files

---
+++ first_program.py
@@ -0,0 +1,41 @@
+6 + 6
+print(3+4)
+
+5 < 10
+print(5 < 10)
+
+"Hello" + " " + "World"
+
+print('he said he is coming')
+print('he said he\'s coming')
+
+print("He said, \"hey\".")
+
+print('one line\ntwo line')
+
+print('this is a backslash: \\')
+
+# what if you don't like the escape character?
+#instead of print("He said, \"hey\".")
+print('He said, "hey".')
+
+print('''he said, "I'll be there".''')
+
+many_lines_string = '''This is a string
+that goes over
+many lines.'''
+
+print(many_lines_string)
+
+print('a')
+print() # prints a blank line
+print('b')
+
+# this is an example of using a named parameter
+print('c', end='----')
+print('d')
+
+# this is using print with a single argument
+print('Hello' + ' ' + 'World')
+# this is using print with 3 arguments
+print('Hello', 'World')
